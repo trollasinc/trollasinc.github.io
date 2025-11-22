3bfebdc95db26a3cb11103ae8d4d6a8ab49072f3
trollasinc <trollasinc2024@gmail.com>
2025-11-22 13:23:31 +0000

    BETA v3.0 (Version en BETA, puede tener errores)

--- translate.js
+++ translate.js
@@ -1,9 +1,11 @@
+// Detecta idioma del navegador y añade hash si no existe
 const lang = (navigator.language || "es").slice(0, 2);
 const supported = ["es", "en", "fr", "it"];
 const finalLang = supported.includes(lang) ? lang : "en";
 
 if (!location.hash) location.hash = finalLang;
 
+// Inicializa Google Translate
 function googleTranslateElementInit() {
   new google.translate.TranslateElement(
     {
@@ -13,3 +15,16 @@ function googleTranslateElementInit() {
     "google_translate_element"
   );
 }
+
+// Forzar traducción según hash automáticamente
+const interval = setInterval(() => {
+  const select = document.querySelector(".goog-te-combo");
+  if (select) {
+    const hashLang = location.hash.slice(1);
+    if (hashLang && select.value !== hashLang) {
+      select.value = hashLang;
+      select.dispatchEvent(new Event("change"));
+    }
+    clearInterval(interval);
+  }
+}, 500);
